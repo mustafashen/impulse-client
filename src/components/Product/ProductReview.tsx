e036cfc06624b418b2e47797ee48e9cd90334c0f
mustafa <mustafasen@tuta.io>
2023-11-30 19:52:15 +0300

    added reviews

--- src/components/Product/ProductReview.tsx
+++ src/components/Product/ProductReview.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
+import { CardStackIcon, StarFilledIcon } from '@radix-ui/react-icons'
 
 export default function ProductReview({reviews}: {reviews: any}) {
   
-  console.log(reviews)
   return (
-    <div>ProductReview</div>
+    <div>
+      {
+        reviews.map((review: any) => {
+          return (
+            <Card key={review.id}>
+              <CardHeader>
+                <CardTitle>Fetch customer name</CardTitle>
+                <div className='flex flex-row flex-nowrap'>
+                {
+                  Array(review.rating).fill(<StarFilledIcon/>).map((star: any) => star)
+                }
+                </div>
+              </CardHeader>
+              <CardContent>
+                {review.comment}
+              </CardContent>
+            </Card>
+          )
+        })
+      }
+    </div>
   )
 }
