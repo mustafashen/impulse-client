bea6068ba7f2ad97e381f9176fbbb10c1c5d55fe
mustafa <mustafasen@tuta.io>
2024-01-03 13:44:24 +0300

    changed carousel

--- src/components/Product/ProductCarousel.tsx
+++ src/components/Product/ProductCarousel.tsx
@@ -1,30 +1,48 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import Carousel from "nuka-carousel"
 import { getProductImages } from '@/lib/s3/fetchProductImages'
 import Image from 'next/image'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from '../ui/carousel'
+import { Card, CardContent } from '../ui/card'
 
 
 export default function ProductCarousel({images} : {images: string[]}) {
 
   return (
     <div>
+      {/* <Carousel>
+  <CarouselContent>
+    <CarouselItem>...</CarouselItem>
+    <CarouselItem>...</CarouselItem>
+    <CarouselItem>...</CarouselItem>
+  </CarouselContent>
+  <CarouselPrevious />
+  <CarouselNext />
+</Carousel> */}
       <Carousel >
+        <CarouselContent>
         {
           images.map((image: string, index: number) => {
             return (
-              <div 
+              <CarouselItem 
                 key={index}
                 className='flex flex-row justify-center items-center'>
-                <Image 
-                src={image} 
-                alt={image}
-                width={300}
-                height={300} />
-              </div>
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <Image 
+                    src={image} 
+                    alt={image}
+                    width={300}
+                    height={300} />
+                  </CardContent>
+                </Card>
+              </CarouselItem>
             )
           })
         }
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
       </Carousel>
     </div>
   )
